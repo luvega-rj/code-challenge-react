3c1387b60b15129128a05eb61555b186fea9d1c9
luvega-rj <luvegaronny@gmail.com>
2023-04-25 06:33:13 +0300

    completed assignment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Form from './Form';
+import Table from './Table';
 import './App.css';
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/transactions')
+      .then(response => response.json())
+      .then(data => setTransactions(data));
+  }, []);
+
+  const handleSort = (type) => {
+    const sortedTransactions = [...transactions].sort((a, b) => {
+      if (a[type] < b[type]) {
+        return -1;
+      }
+      if (a[type] > b[type]) {
+        return 1;
+      }
+      return 0;
+    });
+    setTransactions(sortedTransactions);
+  };
+
+  const handleDelete = (id) => {
+    const filteredTransactions = transactions.filter(transaction => transaction.id !== id);
+    setTransactions(filteredTransactions);
+  };
+
+  const handleAddTransaction = (newTransaction) => {
+    setTransactions([...transactions, newTransaction]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       
+      <Table
+        transactions={transactions}
+        handleSort={handleSort}
+        handleDelete={handleDelete}
+      />
+      <Form handleAddTransaction={handleAddTransaction} />
     </div>
   );
 }
